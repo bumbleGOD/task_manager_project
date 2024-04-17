3929cffb0d0e6f84ec74df8534fa146d921535c8
bumbleGOD <juanjosetm.22@gmail.com>
2024-04-17 09:49:07 -0500

    resolvi problemas con la dashboard del admin (problemas de responsive y estilo en general) ademas de integrar bootstrap para mas maniobrabilidad

--- app/view/assets/scripts/admin/index.js
+++ app/view/assets/scripts/admin/index.js
@@ -1,59 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    const menuCollapse = document.getElementById("menu-desplegable");
-    const miniMenuCollapse = document.querySelector(".barra-lateral");
-    const spans = document.querySelectorAll("span");
-    const darkMode = document.querySelector(".switch");
-    const clickDarkMode = document.querySelector(".circulo");
-    const moon = document.getElementById("moon");
-    const navHorizontal = document.getElementById("nav-horizontal");
-    
-    menuCollapse.addEventListener("click", () => {
-        miniMenuCollapse.classList.toggle("mini-barra-lateral");
-        moon.classList.toggle("ocultar-luna");
-    
-        if (miniMenuCollapse.classList.contains("mini-barra-lateral")) {
-            navHorizontal.style.width = "95%";
-            navHorizontal.style.transition = "width 1s ease";
-        } else {
-            navHorizontal.style.width = "82%";
-        }
-    
-        spans.forEach((span) => {
-            span.classList.toggle("hidden");
-        });
+    const botonActualizarTamaño = document.getElementById('boton-actualizar-tamaño');
+    const sidebar = document.querySelector(".sidebar");
+    const span = document.querySelector(".sidebar h2 span");
+
+    botonActualizarTamaño.addEventListener("click", ()=>{
+        sidebar.classList.toggle("mini-sidebar");
+        span.classList.toggle("hidden");
     });
-
-    darkMode.addEventListener("click", ()=>{
-        let body = document.body;
-
-        body.classList.toggle("dark-mode");
-        clickDarkMode.classList.toggle("prendido");
-    });
-});// esto es para la sidebar y la nav horizontal
-
-let cajaActiva = null;
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Mostrar el contenido inicial al cargar la página
-    mostrar('inicio');
 });
-
-function mostrar(id) {
-    if (cajaActiva) {
-        cajaActiva.style.display = 'none';
-        const linkActivo = document.querySelector('.navegacion-focus');
-        if (linkActivo) {
-            linkActivo.classList.remove('navegacion-focus');
-        }
-    }
-
-    const mostrarCaja = document.getElementById(id);
-    const link = document.querySelector(`a[href="#"][onclick="mostrar('${id}')"]`);
-    if (mostrarCaja && link) {
-        mostrarCaja.style.display = 'block';
-        cajaActiva = mostrarCaja;
-        link.classList.add('navegacion-focus');
-    }
-}
-
-
